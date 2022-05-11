24dff39f9762735074a7b012daef5667ec1f7344
Sanchit Gandhi <93869735+sanchit-gandhi@users.noreply.github.com>
2022-05-11 09:54:06 +0100

    Update run_ctc_dummy.sh

--- run_ctc_dummy.sh
+++ run_ctc_dummy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 python ./run_flax_speech_recognition_ctc.py \
-	--model_name_or_path="/home/patrick_huggingface_co/flax-wav2vec2-large-lv60-scan" \
+	--model_name_or_path="speech-seq2seq/flax-wav2vec2-large-lv60-scan" \
 	--dataset_name="librispeech_asr" \
 	--dataset_cache_dir="/mnt/disks/persist" \
 	--text_column="text" \
