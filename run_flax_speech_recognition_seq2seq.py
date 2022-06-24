6279fec49a6ed8180cf64cb307dea4b8df21fd8f
sanchit-gandhi <sanchit@huggingface.co>
2022-06-24 12:44:49 +0100

    [Dataloader] Handle incomplete final batch

--- run_flax_speech_recognition_seq2seq.py
+++ run_flax_speech_recognition_seq2seq.py
@@ -19,6 +19,7 @@ Fine-tuning the Flax library models for sequence to sequence speech recognition.
 # You can also adapt this script on your own sequence to sequence task. Pointers for this are left as comments.
 
 import logging
+import math
 import os
 import re
 import sys
@@ -39,7 +40,7 @@ import optax
 import transformers
 import wandb as wandb
 from flax import core, jax_utils, struct, traverse_util
-from flax.jax_utils import unreplicate
+from flax.jax_utils import pad_shard_unpad, unreplicate
 from flax.training.common_utils import get_metrics, onehot, shard, shard_prng_key
 from huggingface_hub import Repository
 from models.modeling_flax_speech_encoder_decoder import FlaxSpeechEncoderDecoderModel
@@ -587,14 +588,10 @@ def get_grouped_indices(
 
 def generate_batch_splits(samples_idx: np.ndarray, batch_size: int) -> np.ndarray:
     """Generate batches of data for a specified batch size from sample indices. If the dataset size is not divisible by
-    the batch size, the last incomplete batch is dropped."""
+    the batch size, the last incomplete batch is returned."""
     num_samples = len(samples_idx)
-    samples_to_remove = num_samples % batch_size
-
-    if samples_to_remove != 0:
-        samples_idx = samples_idx[:-samples_to_remove]
-    sections_split = num_samples // batch_size
-    return samples_idx.reshape((sections_split, batch_size))
+    sections_split = math.ceil(num_samples / batch_size)
+    return np.array_split(samples_idx, sections_split)
 
 
 def data_loader(dataset, batch_size, rng, sampler, collator):
@@ -1351,23 +1348,22 @@ def main():
                 samples = [vectorized_datasets["eval"][int(idx)] for idx in batch_idx]
                 batch = data_collator(samples)
                 eval_ids.extend(batch.pop("input_ids"))
-                batch = shard(batch.data)
                 labels = batch["labels"]
 
-                metrics = p_eval_step(state.params, batch)
+                metrics = pad_shard_unpad(p_eval_step)(state.params, batch.data)
                 eval_metrics.append(metrics)
 
                 # generation
                 if training_args.predict_with_generate:
                     if not final_step:
-                        generated_ids = p_generate_step(state.params, batch)
+                        generated_ids = pad_shard_unpad(p_generate_step)(state.params, batch.data)
                         eval_preds.extend(
                             jax.device_get(
                                 generated_ids.reshape(-1, gen_kwargs["num_beams"], gen_kwargs["max_length"])
                             )
                         )
                     else:
-                        generated_ids = p_final_generate_step(state.params, batch)
+                        generated_ids = pad_shard_unpad(p_final_generate_step)(state.params, batch.data)
                         eval_preds.extend(
                             jax.device_get(
                                 generated_ids.reshape(
@@ -1375,7 +1371,7 @@ def main():
                                 )
                             )
                         )
-                    eval_labels.extend(jax.device_get(labels.reshape(-1, labels.shape[-1])))
+                    eval_labels.extend(labels)
 
             # normalize eval metrics
             eval_metrics = get_metrics(eval_metrics)
@@ -1451,8 +1447,7 @@ def main():
                 samples = [vectorized_datasets["train"][int(idx)] for idx in batch_idx]
                 batch = data_collator(samples)
                 batch.pop("input_ids")
-                batch = shard(batch.data)
-                state, train_metric = p_train_step(state, batch)
+                state, train_metric = pad_shard_unpad(p_train_step)(state, batch.data)
 
                 cur_step = epoch * (num_train_samples // batch_size_per_update) + step
 
@@ -1510,21 +1505,20 @@ def main():
                 samples = [vectorized_datasets[split][int(idx)] for idx in batch_idx]
                 batch = data_collator(samples)
                 pred_ids.extend(batch.pop("input_ids"))
-                batch = shard(batch.data)
                 labels = batch["labels"]
 
-                metrics = p_eval_step(state.params, batch)
+                metrics = pad_shard_unpad(p_eval_step)(state.params, batch.data)
                 pred_metrics.append(metrics)
 
                 # generation
                 if training_args.predict_with_generate:
-                    generated_ids = p_final_generate_step(state.params, batch)
+                    generated_ids = pad_shard_unpad(p_final_generate_step)(state.params, batch.data)
                     pred_generations.extend(
                         jax.device_get(
                             generated_ids.reshape(-1, final_gen_kwargs["num_beams"], final_gen_kwargs["max_length"])
                         )
                     )
-                    pred_labels.extend(jax.device_get(labels.reshape(-1, labels.shape[-1])))
+                    pred_labels.extend(labels)
 
             # normalize eval metrics
             pred_metrics = get_metrics(pred_metrics)
