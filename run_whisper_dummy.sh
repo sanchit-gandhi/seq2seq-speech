1dc6edf0a57da8fff41cbf43588b1141de47f0cc
Patrick von Platen <patrick.v.platen@gmail.com>
2022-09-22 02:47:46 +0000

    up

---
+++ run_whisper_dummy.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+CUDA_VISIBLE_DEVICES=1 python run_speech_recognition_whisper.py \
+	--model_name_or_path="small.en" \
+	--dataset_name="hf-internal-testing/librispeech_asr_dummy" \
+	--num_train_epochs="1" \
+	--evaluation_strategy="epoch" \
+	--dataset_config_name="clean" \
+	--train_split_name="validation[:32]" \
+	--eval_split_name="validation" \
+	--test_split_name="validation[:90%]" \
+	--text_column_name="text" \
+	--output_dir="./output_dir" \
+	--run_name="whisper-ls-dummy" \
+	--wandb_project="whisper-dummy" \
+	--per_device_train_batch_size="8" \
+	--per_device_eval_batch_size="4" \
+	--logging_steps="1" \
+	--learning_rate="1e-4" \
+	--warmup_steps="3" \
+	--report_to="wandb" \
+	--push_to_hub="False" \
+	--preprocessing_num_workers="4" \
+	--evaluation_strategy="epoch" \
+	--max_eval_samples="8" \
+	--max_predict_samples="8" \
+	--length_column_name="input_lengths" \
+	--save_strategy="no" \
+	--group_by_length \
+	--overwrite_output_dir \
+	--fp16 \
+	--freeze_encoder \
+	--do_eval \
+	--do_predict \
+	--predict_with_generate \
+	--generation_max_length=64 \
